475b8e399e89bf530e749d0acad1bf9e89ef6d8b
damienorset <42829070+damienorset@users.noreply.github.com>
2018-09-07 14:53:37 +0200

    test

--- End_to_end_ML_with_TF/courseBase.sh
+++ End_to_end_ML_with_TF/courseBase.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-
+touch /root/test.123
 git clone https://github.com/GoogleCloudPlatform/training-data-analyst.git
 cd training-data-analyst/courses/machine_learning/deepdive/06_structured
 apt -y install python3-pip
