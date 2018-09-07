5dd2b1fb0d637a4b431672df5486692134da8ebf
ORSET <damien.orset@atos.net>
2018-09-07 14:22:50 +0200

    add start script

--- End_to_end_ML_with_TF/courseBase.sh
+++ End_to_end_ML_with_TF/courseBase.sh
@@ -1,3 +1,5 @@
+#/bin/bash
+
 git clone https://github.com/GoogleCloudPlatform/training-data-analyst.git
 cd training-data-analyst/courses/machine_learning/deepdive/06_structured
 apt -y install python3-pip
