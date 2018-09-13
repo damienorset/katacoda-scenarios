cf471bd9ff3929c6fa27c4c5c17f8b9d04183c4e
damienorset <42829070+damienorset@users.noreply.github.com>
2018-09-13 10:46:23 +0200

    add ubuntu location

--- End_to_end_ML_with_TF/assets/init_environment.sh
+++ End_to_end_ML_with_TF/assets/init_environment.sh
@@ -4,5 +4,6 @@ apt -y install python3-pip
 pip3 install jupyter pandas datalab
 jupyter notebook --generate-config
 sed -i "s/#c.NotebookApp.password = u''/c.NotebookApp.password = u'sha1:6c2164fc2b22:ed55ecf07fc0f985ab46561483c0e888e8964ae6'/g" /home/scrapbook/.jupyter/jupyter_notebook_config.py 
+#sed -i "s/#c.NotebookApp.password = ''/c.NotebookApp.password = u'sha1:6c2164fc2b22:ed55ecf07fc0f985ab46561483c0e888e8964ae6'/g"  /root/.jupyter/jupyter_notebook_config.py
 git clone https://github.com/GoogleCloudPlatform/training-data-analyst.git
 
