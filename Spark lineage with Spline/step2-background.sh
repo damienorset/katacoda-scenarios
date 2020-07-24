7cde4650919b2acdd849f5182da0308bd9b8e293
Damien O <damien.orset@atos.net>
2020-07-24 19:23:41 +0200

    add coursedata for step 2

---
+++ Spark lineage with Spline/step2-background.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir project
+mkdir project/src
+mkdir project/src/main
+mkdir project/src/main/java
+mkdir project/src/main/resources
+
+touch project/pom.xml
+touch project/src/main/java/SparkAppMain.java
+touch project/src/main/resources/spline.properties
